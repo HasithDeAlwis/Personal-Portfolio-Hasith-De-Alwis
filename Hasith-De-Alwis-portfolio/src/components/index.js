3af7f8d1ce746789e0158d83bb295be3150ce70e
HasithDeAlwis <hasithde24@gmail.com>
2023-10-23 14:57:15 -0400

    Components
    Added react components to the file

---
+++ Hasith-De-Alwis-portfolio/src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import Button from './Button';
+import MainSection from './MainSection';
+import Feedback from './Feedback';
+import Projects from './Projects';
+
+export {
+    Navbar, 
+    Button, 
+    MainSection,
+    Feedback,
+    Projects,
+};
