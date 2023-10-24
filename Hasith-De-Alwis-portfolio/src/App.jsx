d741b5c7111bb39eaf9af4b293cdd5a874119f1c
HasithDeAlwis <hasithde24@gmail.com>
2023-10-24 14:51:54 -0400

    Moved around files to accomodate for pages

--- Hasith-De-Alwis-portfolio/src/App.jsx
+++ Hasith-De-Alwis-portfolio/src/App.jsx
@@ -3,29 +3,11 @@ import styles from './styles';
 import { Navbar, Feedback, MainSection, Projects, Button} from './components';
 /**Weird bug occurs when I import Footer from the es6 deconstruction line above */
 import Footer from './components/Footer.jsx'
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <div className='bg-primary w-full min-h-screen overflow-hidden flex flex-col'>
-      <div className = {`${styles.paddingX} bg-secondary ${styles.flexCenter}`}>
-        <div className = {`${styles.boxWidth}`}>
-          <Navbar />
-        </div>
-      </div>
-
-      <div className = {`bg-primary flex-1 ${styles.flexStart}`}>
-        <div className = {`${styles.boxWidth}`}>
-          <MainSection />
-        </div>
-      </div>
-
-      <div className = {`bg-moonStone ${styles.paddingX} h-9 ${styles.flexStart}`}>
-        <div className = {`${styles.boxWidth}`}>
-          <Footer />
-        </div>
-      </div> 
-    </div>
-    
+    <HomePage />
   );
 }
 
---
+++ Hasith-De-Alwis-portfolio/src/pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from '../styles';
+import { Navbar, Feedback, MainSection, Projects, Button} from '../components';
+/**Weird bug occurs when I import Footer from the es6 deconstruction line above */
+import Footer from '../components/Footer.jsx'
+
+const HomePage = () => {
+  return (
+    <div className='bg-primary w-full min-h-screen overflow-hidden flex flex-col'>
+      <div className = {`${styles.paddingX} bg-secondary ${styles.flexCenter}`}>
+        <div className = {`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className = {`bg-primary flex-1 ${styles.flexStart}`}>
+        <div className = {`${styles.boxWidth}`}>
+          <MainSection />
+        </div>
+      </div>
+
+      <div className = {`bg-moonStone ${styles.paddingX} h-9 ${styles.flexStart}`}>
+        <div className = {`${styles.boxWidth}`}>
+          <Footer />
+        </div>
+      </div> 
+    </div>
+    
+  );
+}
+
+export default HomePage
